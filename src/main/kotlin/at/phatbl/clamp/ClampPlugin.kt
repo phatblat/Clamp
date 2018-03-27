358ec6534d555bcd91e27199b36abdba8159688a
Ben Chatelain <ben@octop.ad>
2018-03-26 20:41:29 -0600

    🍎🐧🏁✅ Wire up OS-appropriate delete task as wrapper finalizer

--- src/main/kotlin/at/phatbl/clamp/ClampPlugin.kt
+++ src/main/kotlin/at/phatbl/clamp/ClampPlugin.kt
@@ -1,14 +1,35 @@
 package at.phatbl.clamp
 
+import at.phatbl.clamp.tasks.wrapper.DeleteBatchFileTask
+import at.phatbl.clamp.tasks.wrapper.DeleteShellScriptFileTask
 import at.phatbl.clamp.tasks.wrapper.WrapperTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-class ClampPlugin: Plugin<Project> {
+/**
+ * Get DRY by clamping down on boilerplate in your Gradle scripts.
+ */
+class ClampPlugin : Plugin<Project> {
+    /** Reference to the project being configured. */
     lateinit var project: Project
+
+    /**
+     * Applies the Clamp plugin to the project.
+     */
     override fun apply(nullableProject: Project?) {
         project = nullableProject ?: return
 
-        project.tasks.create("wrapper", WrapperTask::class.java)
+        val wrapper = project.tasks.create("wrapper", WrapperTask::class.java)
+
+        val os = System.getProperty("os.name")
+        println(os)
+        val deleteFileTask = when (os.toLowerCase().contains("win")) {
+            // Windows
+            true ->
+                project.tasks.create("deleteShellScript", DeleteShellScriptFileTask::class.java)
+            false ->
+                project.tasks.create("deleteBatchFile", DeleteBatchFileTask::class.java)
+        }
+        wrapper.finalizedBy(deleteFileTask)
     }
 }

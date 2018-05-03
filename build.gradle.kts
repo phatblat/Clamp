f1530fa75f1d25eecffa3b537ccbc38f95aa70ac
Ben Chatelain <ben@octop.ad>
2018-05-03 09:36:54 -0600

    ⬆️ Kotlin (1.2.41)

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ plugins {
     `maven-publish`
 
     // Gradle plugin portal - https://plugins.gradle.org/
-    kotlin("jvm") version "1.2.31"
+    kotlin("jvm") version "1.2.41"
     id("com.jfrog.bintray") version "1.8.0"
     id("com.gradle.plugin-publish") version "0.9.10"
 
